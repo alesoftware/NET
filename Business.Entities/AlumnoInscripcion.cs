1b81e545514bffc0c81d7079449e29bf71f82337
Alejandro <alemail@usa.net>
2019-06-03 18:17:38 -0300

    Junio 02

---
+++ Business.Entities/AlumnoInscripcion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class AlumnoInscripcion:BusinessEntity
+    {
+        private int _IDAlumno;
+
+        public int Alumno
+        {
+            get { return _IDAlumno; }
+            set { _IDAlumno = value; }
+        }
+
+        private int _IDCurso;
+
+        public int IDCurso
+        {
+            get { return _IDCurso; }
+            set { _IDCurso = value; }
+        }
+
+        private int _Nota;
+
+        public int Nota
+        {
+            get { return _Nota; }
+            set { _Nota = value; }
+        }
+
+        private string _Condicion;
+
+        public string Condicion
+        {
+            get { return _Condicion; }
+            set { _Condicion = value; }
+        }
+
+
+    }
+}
---
+++ Business.Entities/Materia.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class Materia:BusinessEntity
+    {
+        private string _Descripcion;
+
+        public string Descripcion
+        {
+            get { return _Descripcion; }
+            set { _Descripcion = value; }
+        }
+
+        private int _HSSemanales;
+
+        public int HSSemanales
+        {
+            get { return _HSSemanales; }
+            set { _HSSemanales = value; }
+        }
+
+        private int _HSAnuales;
+
+        public int HSAnuales
+        {
+            get { return _HSAnuales; }
+            set { _HSAnuales = value; }
+        }
+
+        private int _IDPlan;
+
+        public int IDPlan
+        {
+            get { return _IDPlan; }
+            set { _IDPlan = value; }
+        }
+
+
+    }
+}
